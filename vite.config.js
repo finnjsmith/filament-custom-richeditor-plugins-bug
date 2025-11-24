c03e2405106af401b0359907787cc76915b66ad5
Finn Smith <finn.smith@amperative.com>
2025-11-24 09:40:33 +0000

    build: install & run prettier, update all dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { filamentPluginBuild, filamentPluginDevBuilder } from "./bin/build.js";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
